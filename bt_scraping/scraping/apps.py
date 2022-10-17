dd701d645a1adb471d6be002a86a792517021283
Sandor Alin <sandor.alin.nicu@gmail.com>
2022-10-17 11:50:45 +0300

    graph - Adding the local time to be shown for the users. Starting to implement schedule task for the scraping service.

--- bt_scraping/scraping/apps.py
+++ bt_scraping/scraping/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ScrapingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scraping'
+    
+    # def ready(self):
+    #     from scraping.services import scheduler
+    #     scheduler.start()
