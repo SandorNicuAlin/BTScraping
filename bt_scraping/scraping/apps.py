d45f0119cfa5a05be5997940066004ce5187969c
Sandor Alin <sandor.alin.nicu@gmail.com>
2022-10-18 10:47:39 +0300

    create script that runs the scraping method

--- bt_scraping/scraping/apps.py
+++ bt_scraping/scraping/apps.py
@@ -5,6 +5,3 @@ class ScrapingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scraping'
     
-    # def ready(self):
-    #     from scraping.services import scheduler
-    #     scheduler.start()
