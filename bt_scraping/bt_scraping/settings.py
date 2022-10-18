8f83bca02a9fd1c422a24433f6b3e1ae3b6edb46
Sandor Alin <sandor.alin.nicu@gmail.com>
2022-10-18 18:01:09 +0300

    config logging

--- bt_scraping/bt_scraping/settings.py
+++ bt_scraping/bt_scraping/settings.py
@@ -147,7 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGGING = {
     "version": 1,
-    "disable_existing_loggers": True,
+    "disable_existing_loggers": False,
     "root": {"level": "WARNING", "handlers": ["file"]},
     "handlers": {
         "file": {
