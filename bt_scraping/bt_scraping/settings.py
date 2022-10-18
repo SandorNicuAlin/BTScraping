a9caf49e96ffdb628e44eaaac14c2b8fbb6d436c
Sandor Alin <sandor.alin.nicu@gmail.com>
2022-10-18 15:04:27 +0300

    add logging configuration

--- bt_scraping/bt_scraping/settings.py
+++ bt_scraping/bt_scraping/settings.py
@@ -139,3 +139,21 @@ try:
 except Exception as e:
     pass
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': './log/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
