9b75cc8006efda9ebe20fe3cd0e46c5965a53f9e
Sandor Alin <sandor.alin.nicu@gmail.com>
2022-10-18 15:36:51 +0300

    config logging again

--- bt_scraping/bt_scraping/settings.py
+++ bt_scraping/bt_scraping/settings.py
@@ -142,18 +142,32 @@ try:
 except Exception as e:
     pass
 
-LOGGING = {                                                                                                                 
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'logfile': {
-            'class': 'logging.FileHandler',
-            'filename': 'server.log',
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": os.path.join(BASE_DIR, 'logs/django.log'),
+            "formatter": "app",
         },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['logfile'],
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                u"%(asctime)s [%(levelname)-8s] "
+                "(%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
         },
     },
 }
